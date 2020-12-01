b3668bca3c9affdbc2db8ac1c078ace7dcee116b
yiguowu <yiguo.wu@lukka.tech>
2020-12-01 11:04:28 -0500

    Add subtraction

--- lib/generator.go
+++ lib/generator.go
@@ -15,6 +15,13 @@ func (gen *Generator) Init(operator string, limit int) {
 	gen.Operator = operator
 	gen.Range = limit
 }
+
+func swap(a, b *int) {
+	c := *a
+	*a = *b
+	*b = c
+}
+
 func (gen *Generator) Generate(Operand int) string {
 	rand.Seed(time.Now().UTC().UnixNano())
 	switch gen.Operator {
@@ -28,6 +35,13 @@ func (gen *Generator) Generate(Operand int) string {
 			}
 		}
 		return ret + "="
+	case "-":
+		first := rand.Intn(gen.Range)
+		second := rand.Intn(gen.Range)
+		if first < second {
+			swap(&first, &second)
+		}
+		return strconv.Itoa(first) + gen.Operator + strconv.Itoa(second) + "="
 	default:
 		return ""
 	}
